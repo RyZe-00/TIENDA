7e974bebf76456bc817e4c1b585825f305ed709c
unas fiis <unasfiis@Desarrollo.myguest.virtualbox.org>
2024-12-26 14:32:19 -0500

    CREANDO USUARIO ADMIN

---
+++ config/routes.rb
@@ -0,0 +1,27 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  resources :cart_products
+  resources :product_images
+  resources :product_reviews
+  resources :product_categories
+  resources :payments
+  resources :order_items
+  resources :order_states
+  resources :user_addresses
+  resources :user_roles
+  devise_for :users
+  resources :reports
+  resources :carts
+  resources :products
+  resources :orders
+  resources :users
+  resources :categories
+  resources :payment_methods
+  resources :payment_states
+  resources :roles
+  resources :states
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
